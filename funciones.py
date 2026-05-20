37fc63675ddf0391a0ecd23ee8488cb74e9ed5cd
ignacioarnoldt <rocofoco07@gmail.com>
2026-05-20 12:49:36 -0300

    Consigna 2

--- funciones.py
+++ funciones.py
@@ -1,9 +1,18 @@
-x = 10
+# Funcion A
+def A(x):
+    return 40 * x + 200
 
-A = 40 * x + 200
-B = 70 * x + 50
-C = -2 * x**2 + 80 * x + 100
+# Funcion B
+def B(x):
+    return 70 * x + 50
 
-print(A)
-print(B)
-print(C)
+# Funcion C
+def C(x):
+    return -2 * x**2 + 80 * x + 100
+
+
+print("Resultados:")
+
+print(A(10))
+print(B(10))
+print(C(10))
